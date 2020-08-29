d3c2be309bfed13c8ea209f0b520f99445a8675e
Marcio Junior <mdcj1234@gmail.com>
2020-08-29 15:29:07 -0300

    Ajuste no s3 storage provider

--- src/global/container/providers/Storage/implementations/S3StorageProvider.ts
+++ src/global/container/providers/Storage/implementations/S3StorageProvider.ts
@@ -11,7 +11,7 @@ class S3StorageProvider implements IStorageProvider {
 
   constructor() {
     this.client = new aws.S3({
-      region: 'us-east-1',
+      region: process.env.AWS_DEFAULT_REGION,
     });
   }
 
